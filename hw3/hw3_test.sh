6249542b10add7260374d621acea3d5d62c38c7f
zeus7777777 <zeus840703@gmail.com>
2017-11-17 15:55:41 +0800

    fix tar

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,7 +1,7 @@
 cp $1 ensemble/$1
 cd ensemble/
 wget http://cmlab.csie.ntu.edu.tw/~zeus/model.tar
-tar xcf model.tar
+tar xvf model.tar
 python3 vgg11.py $1 11.txt
 python3 vgg13.py $1 13.txt
 python3 vgg16.py $1 16.txt
